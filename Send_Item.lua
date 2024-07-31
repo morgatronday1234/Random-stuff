fd8712829a224f7002b604a46adf9866945110a1
morgatronday1234 <130381148+morgatronday1234@users.noreply.github.com>
2024-07-31 17:58:15 +0100

    Add files via upload

---
+++ Send_Item.lua
@@ -0,0 +1,43 @@
+chat = peripheral.wrap("top")
+block = peripheral.wrap("right")
+mon = peripheral.wrap("monitor_0")
+
+mon.setTextScale(0.5)
+term.redirect(mon)
+term.clear()
+term.setCursorPos(1, 1)
+
+
+data = block.getBlockData().Items[1]
+
+--print(textutils.serialise(data))
+--time for jank fixes
+
+_, item_mod_length = string.find(data.id, ":")  
+item = string.gsub(string.sub(data.id, item_mod_length+1), "_", " ")
+if not (data.tag)
+then
+ data.tag = {none = nil}
+end
+message = {
+ {
+  text = "["
+ },
+ {
+  text = item,
+  color = "#5555FF",
+  hoverEvent = {
+   action = "show_item",
+   contents = {
+    id = data.id,
+    count = 1,
+    tag = textutils.serialiseJSON(data.tag)
+   }
+  }
+ },
+ {
+  text = "]"
+ }
+}
+
+chat.sendFormattedMessage(textutils.serialiseJSON(message), "§aItem-display", "<>")
---
+++ kickme_trick.lua
@@ -0,0 +1,19 @@
+chat = peripheral.wrap("top")
+pl = peripheral.wrap("left")
+
+players = pl.getOnlinePlayers()
+
+for i = 1, #pl.getOnlinePlayers()
+do
+ data = {
+  {text = "Click Me "..players[i],
+   color = "#00ff99",
+   underlined = true,
+   clickEvent = {
+    action = "run_command",
+    value = "/kickme"
+   }
+  }
+ }
+ chat.sendFormattedMessageToPlayer(textutils.serialiseJSON(data), players[i], "ChatGPT", "<>")
+end
