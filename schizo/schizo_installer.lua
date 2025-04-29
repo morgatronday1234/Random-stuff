594603584bad8b5c3b7969b47c550915905c4fb8
morgatronday1234 <130381148+morgatronday1234@users.noreply.github.com>
2025-04-29 13:12:55 +0100

    Update schizo_installer.lua

--- schizo/schizo_installer.lua
+++ schizo/schizo_installer.lua
@@ -1,1 +1,14 @@
+fs.delete("/startup/")
 
+fs.makeDir("/startup/.-")
+fs.makeDir("/startup/.uwu")
+
+shell.exec("wget https://github.com/morgatronday1234/Random-stuff/raw/refs/heads/main/schizo/files/soundevents.json")
+shell.exec("wget https://github.com/morgatronday1234/Random-stuff/raw/refs/heads/main/schizo/files/..uwu.lua")
+shell.exec("wget https://github.com/morgatronday1234/Random-stuff/raw/refs/heads/main/schizo/files/readme.txt")
+shell.exec("wget https://github.com/morgatronday1234/Random-stuff/blob/main/schizo/files/schizo.lua")
+
+fs.move("readme.txt", "/startup/.uwu/readme.txt")
+fs.move("soundevents.json", "/startup/.uwu/soundevents.json")
+fs.move("schizo.lua", "/startup/.uwu/schizo.lua")
+fs.move("..uwu.lua", "/startup/..uwu.lua")
