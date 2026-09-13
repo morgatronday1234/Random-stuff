5cab21c640d532f3fc131367d9dc9085e87870cb
Morg <130381148+morgatronday1234@users.noreply.github.com>
2026-09-13 09:11:23 +0100

    Create killMobs.lua

---
+++ killMobs.lua
@@ -0,0 +1,114 @@
+--Made by Morgatron & GlingusMcDingus(@genericdumbcat)
+--https://github.com/morgatronday1234/Random-stuff/
+
+local expect = require("cc.expect").expect
+local modules = peripheral.wrap("back")
+local mobs, laser = modules, modules
+
+function entDist(ent)
+ expect(1, ent, "table")
+ local vec = vector.new(ent.x, ent.y, ent.z)
+ local dist = math.sqrt((vec.x^2)+(vec.y^2)+(vec.z^2))
+ 
+ return dist
+end
+
+--Fake test ent
+--[[
+entDist({
+ ["x"]= 1.5,
+ ["y"]= 6.3,
+ ["z"]= 2.1
+})
+--]]
+--error("BREAK#2")
+
+function getKeysInRange(ents, keys)
+ expect(1, ents, "table")
+ if (keys == nil) then key = true end --Bypass the key if key was not passed
+ 
+ local matchingEnts = {}
+ setmetatable(matchingEnts, {["__index"]=table})
+ 
+ for _, ent in pairs(ents) do
+  if (keys[ent.key] == true) or (key == true) then
+   setmetatable(ent, {["__index"]={["dist"]=entDist}})
+   matchingEnts:insert(ent)
+  end
+ end
+ 
+ return matchingEnts
+end
+
+function getClosestEnt(ents)
+ expect(1, ents, "table")
+ 
+ --print(#ents)
+ local closestEnt = {}
+ local closestEntDist = 32
+ 
+ for _, ent in pairs(ents) do
+  local curEntDist = ent:dist()--Please just fucking work  
+  
+  if (curEntDist < closestEntDist) then
+   closestEntDist = curEntDist
+   closestEnt = ent
+  end
+  --error("BREAK")
+ end
+ 
+ --print(textutils.serialise(closestEnt))
+ return closestEnt
+end
+
+local function targ(ent)
+ expect(1, ent, "table")
+ if not (ent.x) or not (ent.y) or not (ent.z) then
+  error(("Agument #1: Invaild entity: %s"):format(textutils.serialise(ent)))
+ end
+ 
+ ent.x = ent.x +ent.moitonX
+ ent.y = ent.y +ent.moitonY
+ ent.z = ent.z +ent.moitonZ
+ --local shitToShoot = getKeyInRange(modules.sense(), "minecraft:item_frame")
+ local targYaw = math.deg(math.atan2(-ent.x, ent.z))
+ local targPitch = math.deg(math.atan2(ent.y, math.sqrt((ent.x*ent.x)+(ent.z*ent.z))))
+ 
+ --print(("yaw: %s\nPitch: %s\n"):format(targYaw, -targPitch))
+ laser.fire(targYaw, -targPitch, 5, false)
+end
+
+
+function killTarget()
+ local shitToCheck = mobs.sense()
+ local checkedShit = getKeysInRange(shitToCheck, 
+  {
+   --["minecraft:item_frame"]=true,
+   --["minecraft:pig"]=true,
+   ["minecraft:wither"]=true,--Pray this never happens.
+   ["minecraft:creeper"]=true,
+   ["minecraft:zombie"]=true,
+   ["minecraft:skeleton"]=true,
+   ["minecraft:phantom"]=true,
+   ["minecraft:slime"]=true,
+   ["minecraft:cave_spider"]=true,
+   ["minecrafr:breeze"]=true,
+   ["minecraft:boogged"]=true,
+   ["minecraft:wind_charge"]=true,
+   ["minecraft:pillager"]=true,
+   ["minecraft:spider"]=true,
+   ["minecraft:enderman"]=true
+  }
+ ) 
+ local target = getClosestEnt(checkedShit)
+
+ if (target.x) then
+  --Don't look here, For your own sanity.
+  target.y = target.y+ 0.1
+  
+  --print(textutils.serialise(checkedShit))
+  targ(target)
+ else
+  os.sleep(0.1)
+ end
+end
