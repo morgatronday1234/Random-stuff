0429551f64f621d3be61903225f006b9d72b8482
Morg <130381148+morgatronday1234@users.noreply.github.com>
2026-09-14 18:59:17 +0100

    Make it not so empty

--- Replethora-Laser-Turret.lua
+++ Replethora-Laser-Turret.lua
@@ -1,12 +1,28 @@
 --Made by Morgatron & GlingusMcDingus(@genericdumbcat)
 --https://github.com/morgatronday1234/Random-stuff/blob/main/Replethora-Laser-Turret.lua
 
-require(killMobs)
+require(killMobs) --https://github.com/morgatronday1234/Random-stuff/blob/main/killMobs.lua
 local expect = require("cc.expect").expect
 local mobs = peripheral.wrap("right")
 local laser = peripheral.wrap("left")
 
 
 while(true) do
- killTarget()
+ killTarget({
+   --["minecraft:item_frame"]=true,
+   --["minecraft:pig"]=true,
+   ["minecraft:wither"]=true,--Pray this never happens.
+   ["minecraft:creeper"]=true,
+   ["minecraft:zombie"]=true,
+   ["minecraft:skeleton"]=true,
+   ["minecraft:phantom"]=true,
+   ["minecraft:slime"]=true,
+   ["minecraft:cave_spider"]=true,
+   ["minecrafr:breeze"]=true,
+   ["minecraft:boogged"]=true,
+   ["minecraft:wind_charge"]=true,
+   ["minecraft:pillager"]=true,
+   ["minecraft:spider"]=true,
+   ["minecraft:enderman"]=true
+  })
 end
