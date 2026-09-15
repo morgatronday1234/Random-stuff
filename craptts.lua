0fcb7c80a5d1219144748f9d06874c82972ab06e
Morg <130381148+morgatronday1234@users.noreply.github.com>
2026-09-15 13:43:24 +0100

    Update craptts.lua

--- craptts.lua
+++ craptts.lua
@@ -1,9 +1,17 @@
 --https://github.com/morgatronday1234
 --Made by morgatron
 
+local APIKey = ""
 local aukit = require("aukit")
 while(true) do
- local response_from_tts = textutils.unserialiseJSON(http.post("https://api.v8.unrealspeech.com/speech", textutils.serialiseJSON({["Text"] = tostring(read()), ["VoiceId"] = "Noah"}), {["Content-Type"] = "application/json", ["Authorization"] = "Bearer "}).readAll())
+ local response_from_tts = textutils.unserialiseJSON(
+  http.post("https://api.v8.unrealspeech.com/speech", textutils.serialiseJSON({
+   ["Text"] = tostring(read()), 
+   ["VoiceId"] = "Noah"
+  }), {
+   ["Content-Type"] = "application/json", 
+   ["Authorization"] = "Bearer "..APIKey}).readAll()
+ )
  local tts_data_mp3 = http.get({url = response_from_tts.OutputUri, binary = true}).readAll()
 
  local dfpwm_uri = http.post("https://remote.craftos-pc.cc/music/upload", tts_data_mp3).readAll()
