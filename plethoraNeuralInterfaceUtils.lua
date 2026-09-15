2b71ae88f51614e88437dfe415fca32688042bc5
Morg <130381148+morgatronday1234@users.noreply.github.com>
2026-09-15 15:27:20 +0100

    Update plethoraNeuralInterfaceUtils.lua

--- plethoraNeuralInterfaceUtils.lua
+++ plethoraNeuralInterfaceUtils.lua
@@ -61,8 +61,8 @@ function main() while(true) do
  local event, key, held = os.pullEvent("key")
 
   --getMetaTime = os.epoch("utc")
- local playerData = getSelf()
- if not (playerData) then
+ local pass, playerData = getSelf()
+ if not (pass) then
   goto skipCycle --I shouldn't have to fucking use byte code keywords just to get modules to not crash, Im not fucking going to wrap everthing in pcall.
  end
  --getMetaFinish = (os.epoch("utc")-getMetaTime).."ms"
