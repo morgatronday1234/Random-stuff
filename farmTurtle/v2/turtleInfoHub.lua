16bf523df4c05dd8eba699aa5bc0e1a8076870f9
Morg <130381148+morgatronday1234@users.noreply.github.com>
2026-09-19 20:21:31 +0100

    Update turtleInfoHub.lua

--- farmTurtle/v2/turtleInfoHub.lua
+++ farmTurtle/v2/turtleInfoHub.lua
@@ -2,12 +2,12 @@
 --https://github.com/morgatronday1234/Random-stuff/blob/main/Replethora-Laser-Turret.lua
 
 
+local monitor = peripheral.wrap("monitor_1013")
 local modem = peripheral.wrap("modem_6697")
 local scanner = peripheral.wrap("manipulator_103")
 local expect = require("cc.expect")
 
 local deviceId = "provider#1"
-
 --Outbound
 --[[
 {
@@ -20,10 +20,10 @@ local deviceId = "provider#1"
 --]]
 
 
-function getBushes(blocks)
+function getBlocks(blocks, filter)
  local bushes = {}
  for _, block in pairs(blocks) do
-  if (block.name:find("berry")) then
+  if (block.name:find(filter)) then
    table.insert(bushes, block)
   end
  end
@@ -31,21 +31,26 @@ function getBushes(blocks)
  return bushes
 end
 
-
 while(true) do
- local bushes = getBushes(scanner.scan())
- 
+ local blockScan = scanner.scan()
+
+ local bushes = getBlocks(blockScan, "berry")
  local totalReadyBushes = 0 
  for _, bush in pairs(bushes) do
   if (bush.state.age == 3) then
    totalReadyBushes = totalReadyBushes +1
   end
  end 
- term.setCursorPos(1, 1)
- term.clear()
- print(("Berry Bushes Ready: \n%s%% (%s/%s)"):format(math.floor((totalReadyBushes/#bushes)*100), totalReadyBushes, #bushes))
  
- modem.transmit(42424, 0, {["data"] = {["bushesReady"] = totalReadyBushes, ["totalBushes"] = #bushes}, ["deviceId"] = deviceId})
+ local tomatos = getBlocks(blockScan, "tomato")
+ local totalReadyTomatos = 0 
+ for _, tomato in pairs(tomatos) do
+  if (tomato.state.age == 3) then
+   totalReadyTomatos = totalReadyTomatos +1
+  end
+ end 
+
+ modem.transmit(42424, 0, {["data"] = {["bushesReady"] = totalReadyBushes, ["totalBushes"] = #bushes, ["tomatosReady"] = totalReadyTomatos, ["totalTomatos"] = #tomatos}, ["deviceId"] = deviceId})
  os.sleep(3)
 end
 
